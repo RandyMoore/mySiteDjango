60a0faf44bd299dfbf8fa08a4cd4dd50a0a1a6ae
Randy Moore <randal.a.moore@gmail.com>
2017-09-01 10:09:39 -0500

    Restructering for React

--- my_site_django/government_audit/urls.py
+++ my_site_django/government_audit/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^search/$', views.search),
     url(r'^$', views.AuditDocumentList.as_view()),
 ]
--- my_site_django/government_audit/views.py
+++ my_site_django/government_audit/views.py
@@ -1,9 +1,14 @@
+from django.shortcuts import render
 from rest_framework import generics
 
 from .models import AuditDocument
 from .serializers import AuditDocumentSerializer
 
+# Django
+def search(request):
+    return render(request, 'government_audit/audit_search.html', {})
 
+# REST API
 class AuditDocumentList(generics.ListCreateAPIView):
     queryset = AuditDocument.objects.all()
     serializer_class = AuditDocumentSerializer
