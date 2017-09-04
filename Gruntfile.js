ff06846c889e31f3b9ed0e09733c89759d0766d5
Randy Moore <randal.a.moore@gmail.com>
2017-09-04 10:54:37 -0500

    Include Nav Bar

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,13 +13,14 @@ module.exports = function (grunt) {
                // if the source file has an extension of es6 then
                // we change the name of the source file accordingly.
                // The result file's extension is always .js
-               "./my_site_django/static/government_audit/audit_search.js": ["./src/government_audit/audit_search.js"]
+               "./my_site_django/static/government_audit/audit_search.js":
+                 ["./my_site_django/src/government_audit/audit_search.js"]
             }
          }
       },
       watch: {
          scripts: {
-            files: ["./src/**/*.js"],
+            files: ["./my_site_django/src/**/*.js"],
             tasks: ["browserify"]
          }
       }
