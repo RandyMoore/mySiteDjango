5890a8746785541f7a39a8fe8f0b819a84e164c0
Randy Moore <randal.a.moore@gmail.com>
2017-10-16 21:26:31 -0500

    Audit Document parallelize mining

--- my_site_django/upload_audit_docs.py
+++ my_site_django/upload_audit_docs.py
@@ -7,73 +7,98 @@ Create a file pointing to .json text paths
 Then feed that file to this script
 './upload_audit_docs.py reports.txt'
 """
-import os, django, fileinput, json
+from multiprocessing import Pool
+
+import django
+import fileinput
+import json
+import multiprocessing
+import os
+from django.db import transaction
 from government_audit_mining.audit_miner import get_named_entities
 from nltk.probability import FreqDist
 
+def save_to_db(db_queue):
+    while True:
+        doc_tuple = db_queue.get()
+
+        if not doc_tuple:
+            print("DB found None, exiting")
+            return
+
+        doc, named_entities = doc_tuple
+        print('DB PID', os.getpid(), 'saving', doc.title)
+
+        try:
+            doc.save()
+            with transaction.atomic():
+                for entity in named_entities:
+                    entity.document = doc
+                    entity.save()
+        except BaseException as e:
+            print('Failed to save to DB', str(e))
+
+
+def process_documents(args_tuple):
+    path, db_queue = args_tuple
+    meta_path = path.strip('\n')
+    print('Process ', os.getpid(), ':', meta_path)
+
+    if not os.path.exists(meta_path):
+        print('Input path ' + meta_path + ' not found - Skipping')
+        return
+
+    with open(meta_path) as meta_file:
+        meta = json.loads(meta_file.read())
+
+        if not 'url' in meta or not meta['url']:
+            print(f'Skipping {meta["title"]} as url is null')
+            return
+
+        doc = AuditDocument()
+        doc.title = meta['title']
+        doc.publication_date = meta['published_on']
+        doc.source = 'AO'
+        doc.url = meta['url']
+
+        text_path = meta_path.replace('report.json', 'report.txt')
+
+        if os.path.exists(text_path):
+            with open(text_path) as text_file:
+                doc.text = text_file.read()
+        else:
+            print('No file for entry ' + text_path + ' skipping file')
+            return
+
+        # Data mine the doc
+        ne_fdist = FreqDist(ne for ne in get_named_entities(doc.text))
+        named_entities = []
+        for ne in ne_fdist.most_common():
+            if ne[1] < 3:
+                break
+            named_entity = NamedEntity()
+            named_entity.name = ne[0]
+            named_entity.frequency = ne[1]
+            named_entities.append(named_entity)
+
+        db_queue.put((doc, named_entities))
+
 
 if __name__ == "__main__":
-    # enter django shell
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_site_django.settings")
     django.setup()
 
     from government_audit.models import AuditDocument, NamedEntity
 
-    existing_titles = set([record[0] for record in AuditDocument.objects.values_list('title')])
+    # TODO: find a better way to resume after failure.
+    # existing_titles = set([record[0] for record in AuditDocument.objects.values_list('title')])
+    db_queue = multiprocessing.Manager().Queue()
 
-    for i, path in enumerate(fileinput.input()):
-        print('\n', i, path)
-        meta_path = path.strip('\n')
+    files = [f for f in fileinput.input()]
 
-        if not os.path.exists(meta_path):
-            print('Input path ' + meta_path + ' not found - Skipping')
-            continue
+    multiprocessing.Process(target=save_to_db, args=(db_queue,)).start()
 
-        with open(meta_path) as meta_file:
-            meta = json.loads(meta_file.read())
+    with multiprocessing.Pool() as p: # by default creates as many processes as available cores.
+        p.map(func=process_documents, iterable=zip(files, [db_queue]*len(files)))
 
-            if not meta['url']:
-                print(f'Skipping {meta["title"]} as url is null')
-                continue
-
-            if meta['title'] in existing_titles:
-                print('Skipping as title exists in DB ' + meta['title'])
-                continue
-
-            doc = AuditDocument()
-            doc.title = meta['title']
-            doc.publication_date = meta['published_on']
-            doc.source = 'AO'
-            doc.url = meta['url']
-
-            text_path = meta_path.replace('report.json', 'report.txt')
-            text = None
-            if os.path.exists(text_path):
-                with open(text_path) as text_file:
-                    text = text_file.read()
-            else:
-                print('No file for entry ' + text_path + ' skipping file')
-                continue
-
-            doc.text = text # doc.text is cleared after save, keeping text around for analysis
-            try:
-                doc.save()
-                print("Saved " + meta_path)
-            except BaseException as e:
-                print("Failed to save " + meta_path + " : " + str(e))
-
-            # Data mine the doc
-            ne_fdist = FreqDist(ne for ne in get_named_entities(text))
-            for ne in ne_fdist.most_common():
-                if ne[1] < 3:
-                    break
-                named_entity = NamedEntity()
-                named_entity.document = doc
-                named_entity.name = ne[0]
-                named_entity.frequency = ne[1]
-
-                try:
-                    named_entity.save()
-                    print("Saved named entity ", named_entity.name, named_entity.frequency)
-                except BaseException as e:
-                    print("Failed to save named entity " + named_entity.name + " : " + str(e))
+    db_queue.put(None)
