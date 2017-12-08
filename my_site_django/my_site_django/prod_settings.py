37480775ffc974da2884b80fc59b62346e0af6e9
Randal Moore <randal.a.moore@gmail.com>
2017-12-08 17:15:32 +0100

    Allow www.randalmoore.me for https

--- my_site_django/my_site_django/prod_settings.py
+++ my_site_django/my_site_django/prod_settings.py
@@ -2,7 +2,7 @@ from .settings import *
 
 # Override base dev settings for production
 DEBUG = False
-ALLOWED_HOSTS += ['.amazonaws.com','randalmoore.me']
+ALLOWED_HOSTS += ['.amazonaws.com','.randalmoore.me']
 
 if 'DJANGO_SECRET_KEY' in os.environ:
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
